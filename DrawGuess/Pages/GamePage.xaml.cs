54405d087a3182feddc92a62b0ed8387555bf4f2
Gabriella Thorén <gabriella_thoren@hotmail.com>
2019-08-01 16:32:06 +0200

    Fixeed set game mode bug

--- DrawGuess/Pages/GamePage.xaml.cs
+++ DrawGuess/Pages/GamePage.xaml.cs
@@ -129,7 +129,7 @@ namespace DrawGuess.Pages
             {
                 ViewModel.Players.Add(player);
                 SetPlacement();
-                SetGameMode();
+                //SetGameMode();
             }
             catch(Exception)
             {
